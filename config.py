d51492b1ecc678260c13cd57e54fe5ed93241ed5
123465suhail <suhailsuhail73217@gmail.com>
2024-09-04 16:09:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7532153463:AAEcIf0Xf_qTCwlpygOqPD7pfzfw2zXHfzg")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("suhail")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("885864033")
 
 
 
